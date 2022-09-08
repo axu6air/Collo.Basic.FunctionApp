cd902dce9831707f5b9bfc69d628c89537403b4c
axu6air <axu6air@gmail.com>
2022-09-08 19:45:03 +0600

    program.cs file changed

--- Collo.Basic.FunctionApp/Program.cs
+++ Collo.Basic.FunctionApp/Program.cs
@@ -1,7 +1,36 @@
+using Collo.Cloud.Services.Libraries.Shared.Middlewares;
+using Collo.Cloud.Services.Libraries.Shared.Permission;
+using Collo.Cloud.Services.Libraries.Shared.Persistence.Data;
+using Collo.Cloud.Services.Libraries.Shared.Persistence.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureFunctionsWorkerDefaults((builder) =>
+    {
+        builder.UseMiddleware<GlobalExceptionMiddleware>();
+        //builder.UseMiddleware<AuthenticationMiddleware>();
+        //builder.UseMiddleware<AuthorizationMiddleware>();
+    })
+    .ConfigureHostConfiguration(configuration =>
+    {
+        configuration.AddJsonFile("local.settings.json", optional: true);
+    })
+    .ConfigureServices((services) =>
+    {
+        services.AddDataConfigurationService("COSMOSDBCONNECTIONSETTINGS_COMMON_KV");
+
+        services.AddDomainConfigurationService();
+
+        services.AddColloPermission();
+    })
+    .ConfigureLogging((logging) =>
+    {
+        logging.AddConsole();
+        logging.AddDebug();
+    })
     .Build();
 
 host.Run();
+
