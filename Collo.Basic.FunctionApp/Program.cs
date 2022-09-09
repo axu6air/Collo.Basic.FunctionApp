e9e6c9d8f58eece2c93ddae65e8aa73d7ef9951e
axu6air <axu6air@gmail.com>
2022-09-09 10:20:45 +0600

    program file reverted

--- Collo.Basic.FunctionApp/Program.cs
+++ Collo.Basic.FunctionApp/Program.cs
@@ -1,9 +1,50 @@
-using Collo.Cloud.Services.Libraries.Shared.B2C;
+//using Collo.Cloud.Services.Libraries.Shared.B2C;
+//using Collo.Cloud.Services.Libraries.Shared.Middlewares;
+//using Collo.Cloud.Services.Libraries.Shared.Permission;
+//using Collo.Cloud.Services.Libraries.Shared.Persistence.Data;
+//using Collo.Cloud.Services.Libraries.Shared.Persistence.Services;
+//using Collo.Cloud.Services.Libraries.Shared.Seeding;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+
+//var host = new HostBuilder()
+//    .ConfigureFunctionsWorkerDefaults((builder) =>
+//    {
+//        builder.UseMiddleware<GlobalExceptionMiddleware>();
+//        //builder.UseMiddleware<AuthenticationMiddleware>();
+//        //builder.UseMiddleware<AuthorizationMiddleware>();
+//    })
+//    .ConfigureHostConfiguration(configuration =>
+//    {
+//        configuration.AddJsonFile("local.settings.json", optional: true);
+//        //configuration.AddKeyVaultService("KEYVAULTCONFIGURATION_KV");
+//    })
+//    .ConfigureServices((services) =>
+//    {
+//        services.AddDataConfigurationService("COSMOSDBCONNECTIONSETTINGS_COMMON_KV");
+
+//        services.AddB2CConfigurationService("AZUREADB2CSETTINGS_KV");
+
+//        services.AddDomainConfigurationService();
+
+//        services.AddColloPermission();
+
+//        services.AddSeedingData("SEEDING_DATA_COMMON_KV");
+//    })
+//    .ConfigureLogging((logging) =>
+//    {
+//        logging.AddConsole();
+//        logging.AddDebug();
+//    })
+//    .Build();
+
+//host.Run();
+
 using Collo.Cloud.Services.Libraries.Shared.Middlewares;
 using Collo.Cloud.Services.Libraries.Shared.Permission;
 using Collo.Cloud.Services.Libraries.Shared.Persistence.Data;
 using Collo.Cloud.Services.Libraries.Shared.Persistence.Services;
-using Collo.Cloud.Services.Libraries.Shared.Seeding;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -18,19 +59,14 @@ var host = new HostBuilder()
     .ConfigureHostConfiguration(configuration =>
     {
         configuration.AddJsonFile("local.settings.json", optional: true);
-        //configuration.AddKeyVaultService("KEYVAULTCONFIGURATION_KV");
     })
     .ConfigureServices((services) =>
     {
         services.AddDataConfigurationService("COSMOSDBCONNECTIONSETTINGS_COMMON_KV");
 
-        services.AddB2CConfigurationService("AZUREADB2CSETTINGS_KV");
-
         services.AddDomainConfigurationService();
 
         services.AddColloPermission();
-
-        services.AddSeedingData("SEEDING_DATA_COMMON_KV");
     })
     .ConfigureLogging((logging) =>
     {
